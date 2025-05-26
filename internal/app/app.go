aa6e5caf83fc7b49813b1105874221e2bb5ea396
developertomek <tomekcodes@gmail.com>
2025-05-26 11:13:15 +0200

    Add database migrations for users, workouts, and workout entries

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/developertomek/go-api/internal/api"
 	"github.com/developertomek/go-api/internal/store"
+	"github.com/developertomek/go-api/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,11 @@ func NewApplication() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
